637f1cafd4f37bface6eebae6c91e9bef1433a1f
George <paninnskk@gmail.com>
2023-04-04 21:14:02 +0700

    test wrapper

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('.header-burger').click(function (event) {
         $('.header-burger, .header-navigation').toggleClass('active');
-
+        //$('body').toggleClass('fixScroll');
     })
 })
